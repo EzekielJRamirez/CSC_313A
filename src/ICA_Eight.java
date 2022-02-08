8e2109968e7cfacd3bb040b7d0dd2ab6f3dbec16
ezekieljramirez <ezekiel.ramirez@calbaptist.edu>
2022-02-08 14:32:46 -0800

    updated eight

--- src/ICA_Eight.java
+++ src/ICA_Eight.java
@@ -5,7 +5,7 @@ public class ICA_Eight {
     /**
      * time O(n^2)
      * space O(n)
-     *
+     * <p>
      * check optimization on page 179 - 184 or 151 - 160
      */
 
@@ -16,22 +16,44 @@ public class ICA_Eight {
             Integer i = j - 1;
             while (i >= 0 && sortData.elementAt(i) > key) {
                 sortData.set(i + 1, sortData.elementAt(i));
-                i = i -1;
+                i = i - 1;
             }
             sortData.set(i + 1, key);
+        }
+    }
+
+    /**
+     * time: polynomial - O(n * log(n))
+     */
+    public static void recInsertSort(Vector<Integer> sortData, Integer jay) {
+        Integer key = sortData.elementAt(jay);
+        Integer i = jay - 1;
+        while (i >= 0 && sortData.elementAt(i) > key) {
+            sortData.set(i + 1, sortData.elementAt(i));
+            i = i - 1;
+        }
+        sortData.set(i + 1, key);
+
+        // the jay should not be checking for data after the end of the vector
+        // so jay must be < size - 1
+        if (jay < sortData.size() - 1) {
+            jay = jay + 1;
+            recInsertSort(sortData, jay);
         }
     }
 
     public static void main(String[] args) {
         Vector<Integer> myData = new Vector<Integer>();
         Random r = new Random();
+        int size = r.nextInt(10) + 5;
 
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < size; i++) {
             myData.addElement(r.nextInt(30));
         }
 
         System.out.println(myData);
-        insertSort(myData);
+//        insertSort(myData);
+        recInsertSort(myData, 1);
         System.out.println(myData);
     }
 }
