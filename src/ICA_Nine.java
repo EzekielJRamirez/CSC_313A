565af9b9f7fe78b35aaac42592c280b4685d8365
ezekieljramirez <ezekiel.ramirez@calbaptist.edu>
2022-02-12 08:11:32 -0800

    added work to nine

--- src/ICA_Nine.java
+++ src/ICA_Nine.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Vector;
 
 public class ICA_Nine {
@@ -6,7 +7,7 @@ public class ICA_Nine {
         if (p < r) {
             Integer q = (p + r) / 2;
             // do I need to cast to int if Integer naturally
-            // truncates the value for floor round
+            // truncates the value for floor round?
             mergeSort(a, p, q);
             mergeSort(a, q + 1, r);
             merge(a, p, q, r);
@@ -19,18 +20,20 @@ public class ICA_Nine {
         // let L[1...nOne + 1] and R[1...nTwo + 1] be new arrays
         Vector<Integer> left = new Vector<Integer>();
         Vector<Integer> right = new Vector<Integer>();
-        for (int i = 1; i < nOne; i++) {
+        for (int i = 1; i <= nOne; i++) {
             // init L
-            left.addElement(a.elementAt(p + i -1));
+            left.addElement(a.elementAt(p + i - 1));
         }
-        for (int j = 1; j < nTwo; j++) {
+        for (int j = 1; j <= nTwo; j++) {
             // init R
             right.addElement(a.elementAt(q + j));
         }
-        // to infinity?
+        // to infinity? lines 8 and 9 of psuedocode
+        left.addElement(Integer.MAX_VALUE);
+        right.addElement(Integer.MAX_VALUE);
 
         int i = 1;
-        int j = 2;
+        int j = 1;
 
         for (int k = p; k < r; k++) {
             if (left.elementAt(i) <= right.elementAt(i)) {
@@ -45,7 +48,25 @@ public class ICA_Nine {
 
     public static void main(String[] args) {
         Vector<Integer> vInt = new Vector<Integer>();
-        vInt.addElement(20);
-        vInt.addElement(22);
+//        Random r = new Random();
+//        int size = r.nextInt(5) + 10;
+//
+//        for (int i = 0; i < size; i++) {
+//            vInt.addElement(r.nextInt(20));
+//        }
+        vInt.addElement(8);
+        vInt.addElement(0);
+        vInt.addElement(1);
+        vInt.addElement(9);
+        vInt.addElement(5);
+        vInt.addElement(9);
+        vInt.addElement(17);
+        vInt.addElement(19);
+        vInt.addElement(0);
+
+//        8, 0, 1, 9, 5, 9, 5, 17, 19, 0
+        System.out.println(vInt);
+        mergeSort(vInt, 1, vInt.size());
+        System.out.println(vInt);
     }
 }
