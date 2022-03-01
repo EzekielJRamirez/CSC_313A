ab6a493c561b83da27303cb66c561cab611db588
Zecoder <70232378+EzekielJRamirez@users.noreply.github.com>
2022-03-01 12:38:47 -0800

    switched to vector of vectors

--- src/Fourteen.java
+++ src/Fourteen.java
@@ -7,19 +7,48 @@ import java.util.Vector;
 
 public class Fourteen {
 
-    public static void bucketSort() {
-        // add code
+    public static void bucketSort(Vector<Integer> input) {
+        Vector<Vector<Integer>> buck = new Vector<>(10);
+        // input size should be 9 for int and 25 for alphabet
+        Integer n = input.size();
+        for (int i = 0; i < n - 1; i++) {
+            //make B[i] an empty list or an empty vector
+//            buck.addElement(bee.addElement(0));
+//            bee.addElement(0);
+            buck.addElement(new Vector<Integer>());
+        }
+//        buck.addElement(bee);
+        for (int i = 1; i < n; i++) {
+            //insert A[i] into list B[n * A[i]] with floor round
+//            buck.elementAt(bee.elementAt(i)).set(i,input.elementAt(i));
+            Vector<Integer> bee = buck.elementAt(i);
+//            bee
+        }
+        System.out.println(buck);
     }
 
-    public static void insertionSort() {
-        // add code
+//    public static void insertionSort() {
+//        // add code
+//    }
+
+    public static void insertSort(Vector<Integer> input) {
+        for (Integer j = 1; j < input.size(); j++) {
+            Integer key = input.elementAt(j);
+            // insert sortData[j] into A[1, ..., j - 1]
+            Integer i = j - 1;
+            while (i >= 0 && input.elementAt(i) > key) {
+                input.set(i + 1, input.elementAt(i));
+                i = i - 1;
+            }
+            input.set(i + 1, key);
+        }
     }
 
     public static void main(String[] args) {
         Vector<Integer> myData = new Vector<>();
 //        Vector<Integer> bData = new Vector<>();
 //        int temp = 0;
-        Integer vSize = 15;
+        Integer vSize = 5;
         Random r = new Random();
         for (int i = 0; i < vSize; i++) {
             myData.addElement(r.nextInt(100) + 1);
@@ -31,5 +60,6 @@ public class Fourteen {
         }
 
         System.out.println("ICA 13\n" + myData);
+        bucketSort(myData);
     }
 }
